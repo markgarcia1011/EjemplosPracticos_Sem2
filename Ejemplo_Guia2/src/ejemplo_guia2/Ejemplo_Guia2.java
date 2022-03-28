74800affbdb7286768cf16aaec6d69ac6d996516
markgarcia1011 <101343926+markgarcia1011@users.noreply.github.com>
2022-03-28 13:15:15 -0600

    Add files via upload

---
+++ Ejemplo_Guia2/src/ejemplo_guia2/Ejemplo_Guia2.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo_guia2;
+public class Ejemplo_Guia2 {
+
+    public static void main(String[] args) {
+        
+       // Hacer el llamado cada uno de los métodos creados.
+       
+       metodoConVoid();
+       metodoConInt();
+       metodoConDouble();
+       metodoConString();
+       
+    }
+    public static void metodoConVoid() {
+        System.out.println("Contenido del método con void");
+    }
+    public static int metodoConInt () {
+        System.out.println("Contenido del método con int");
+        return 0;
+    }
+    public static double metodoConDouble(){
+        System.out.println("Contenido del método con double");
+        return 0.00;
+    }
+    public static String metodoConString() {
+        System.out.println("Contenido del metodo con String");
+        return "";
+    }
+} 
+
